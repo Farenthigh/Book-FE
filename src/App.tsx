2030e5c117e7f769f50cc7b5aba192d2a039e93f
Farenthigh <naxx.iiax@gmail.com>
2024-10-26 15:26:24 +0700

    🎉 init : Project init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <main className="w-screen h-screen bg-slate-700 flex justify-center items-center">
+        <p className="text-white">Hello world</p>
+      </main>
+    </>
+  );
+}
+
+export default App;
