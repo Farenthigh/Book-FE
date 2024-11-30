87dd094039e9766b0bf91d95eebf8e408227c20c
Farenthigh <naxx.iiax@gmail.com>
2024-12-01 05:32:35 +0700

    adding route, page and adding navbar change when login

--- src/Pages/Homepage.tsx
+++ src/Pages/Homepage.tsx
@@ -1,16 +1,24 @@
-import { useState } from "react";
-import arrowleft from "../assets/arrowleft.png";
-import arrowright from "../assets/arrowright.png";
+import { useContext } from "react";
+import banner4 from "../assets/banner4.png";
+import SaleRentButtom from "../components/Catagories/SaleRentButton";
 import Footer from "../components/Footer/Footer";
+import { AuthContext } from "../context/Auth";
 
 const Homepage = () => {
+  const auth = useContext(AuthContext);
+  console.log(auth);
+
   return (
     <>
-      <div className="p-5 font-serif flex justify-center gap-10">
-        <div>BOOK</div>
-        <div>AUTHOR</div>
-        <div>PUBLISHER</div>
-      </div>
+      {!auth?.auth.isAuth ? (
+        <div className="p-5 font-serif flex justify-center gap-10">
+          <div>BOOK</div>
+          <div>AUTHOR</div>
+          <div>PUBLISHER</div>
+        </div>
+      ) : (
+        <></>
+      )}
 
       <div className="bg-[#f9f9ff] items-center mb-5 px-24 py-8">
         <div className="w-full mx-auto">
