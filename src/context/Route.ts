e2a54ef1999fea119aa06395e56a3b691766308e
Poom2105 <poomkeittipoom@gmail.com>
2024-11-30 16:33:46 +0700

    profile, addPost, editPost

--- src/context/Route.ts
+++ src/context/Route.ts
@@ -2,4 +2,13 @@ export const USER_HOME_ROUTE = "/";
 export const TEST_ROUTE = "/test";
 export const LOGIN_ROUTE = "/login";
 export const SALE_ROUTE = "/sale";
-export const CART_ROUTE = "/cart";
+export const RENT_ROUTE = "/rent";
+export const ALLCAT_ROUTE = "/allCat";
+export const S_DETAIL_ROUTE = "/Sdetail";
+export const R_DETAIL_ROUTE = "/Rdetail";
+export const FAV_ROUTE = "/fav";
+export const PROFILE_ROUTE = "/profile";
+export const ADDPOST_ROUTE = "/addPost";
+export const EDITPOST_ROUTE = "/editPost";
+
+export const SIGNUP_ROUTE = "/signup";
